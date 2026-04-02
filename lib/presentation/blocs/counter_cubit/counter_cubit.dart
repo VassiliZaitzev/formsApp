90712d9a8114b0022d748575e67af398e963e750
James Alfred Johnson Montano <jjohnson@MJOLC3MD>
2026-04-02 14:02:24 -0300

    FC

---
+++ lib/presentation/blocs/counter_cubit/counter_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 5));
+
+
+  
+}
---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cubit screen"),
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.refresh_rounded)
+          )
+        ],
+      ),
+      body: Center(
+        child: const Text("Counter Value"),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: "1",
+            child: const Text("+3"),
+            onPressed: (){}
+          ),
+          const SizedBox(height: 15,),
+          FloatingActionButton(
+            heroTag: "1",
+            child: const Text("+2"),
+            onPressed: (){}
+          ),
+          const SizedBox(height: 15,),
+          FloatingActionButton(
+            heroTag: "1",
+            child: const Text("+1"),
+            onPressed: (){}
+          )
+        ],
+      ),
+    );
+  }
+}
